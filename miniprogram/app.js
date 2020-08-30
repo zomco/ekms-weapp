105c66e3a69fc3740ce8cbb3c9a385be1ac0e256
zomco <zomcoo@gmail.com>
2020-08-30 16:53:01 +0800

    合并代码

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,7 +1,17 @@
 //app.js
 App({
+  buf2hex: function (buffer) {
+    return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('')
+  },
+  buf2string: function (buffer) {
+    var arr = Array.prototype.map.call(new Uint8Array(buffer), x => x)
+    var str = ''
+    for (var i = 0; i < arr.length; i++) {
+      str += String.fromCharCode(arr[i])
+    }
+    return str
+  },
   onLaunch: function () {
-    
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
@@ -14,7 +24,9 @@ App({
         traceUser: true,
       })
     }
-
-    this.globalData = {}
+    this.globalData.SystemInfo = wx.getSystemInfoSync();
+  },
+  globalData: {
+    SystemInfo: {}
   }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,120 +1,274 @@
 //index.js
+//获取应用实例
 const app = getApp()
+const util = require('../../utils/util.js')
+//引进我们的 mqtt 库
+const mqtt = require('../../utils/mqtt.min.js')
+//连接mqtt 的域名，我提供的这个库的域名入参参数是 以 wxs 开头 还支持阿里小程序，后缀的 /mqtt 表示是一种服务
+// 域名 a0je61a.mqtt.iot.gz.baidubce.com
+const host = 'wxs://a0je61a.mqtt.iot.gz.baidubce.com/mqtt'
 
+let colorPickerCtx = {};
+let sliderCtx = {};
+let _this = null
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
-    logged: false,
-    takeSession: false,
-    requestResult: ''
-  },
-
-  onLoad: function() {
-    if (!wx.cloud) {
-      wx.redirectTo({
-        url: '../chooseLib/chooseLib',
-      })
-      return
+    pickColor: null,
+    raduis: 550, //这里最大为750rpx铺满屏幕
+    valueWidthOrHerght: 0,
+    client: null,
+    //记录重连的次数
+    reconnectCounts: 0,
+    //MQTT连接的配置
+    options: {
+      protocolVersion: 4, //MQTT连接协议版本
+      clientId: 'miniTest',
+      clean: false,
+      password: 'OHiLItaGMsEx0cwh',
+      username: 'a0je61a/wechat',
+      reconnectPeriod: 1000, //1000毫秒，两次重新连接之间的间隔
+      connectTimeout: 30 * 1000, //1000毫秒，两次重新连接之间的间隔
+      resubscribe: true //如果连接断开并重新连接，则会再次自动订阅已订阅的主题（默认true）
     }
+  },
+  mqttConnect: function() {
 
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-            }
+    var that = this;
+    //开始连接
+    this.data.client = mqtt.connect(host, this.data.options);
+    this.data.client.on('connect', function(connack) {
+      wx.showToast({
+        title: '连接成功'
+      })
+
+      that.onClickSync()
+    })
+
+
+    //设备端上报消息的回调
+    that.data.client.on("message", function(topic, payload) {
+      let data = JSON.parse(payload)
+  
+      let h = util.rgb2hsl(data.Red, data.Green, data.Blue);
+      util.drawSlider(sliderCtx, _this.data.valueWidthOrHerght, that.data.valueWidthOrHerght, h[0]);
+      that.setData({
+        pickColor: JSON.stringify({
+          red:  data.Red,
+          green:  data.Green,
+          blue: data.Blue
+        })
+      })
+    })
+
+
+    //服务器连接异常的回调
+    that.data.client.on("error", function(error) {
+      console.log(" 服务器 error 的回调" + error)
+
+    })
+
+    //服务器重连连接异常的回调
+    that.data.client.on("reconnect", function() {
+      console.log(" 服务器 reconnect的回调")
+
+    })
+
+
+    //服务器连接异常的回调
+    that.data.client.on("offline", function(errr) {
+      console.log(" 服务器offline的回调")
+
+    })
+
+
+  },
+  mqttSubTopic: function() {
+    if (this.data.client && this.data.client.connected) {
+      //仅订阅单个主题
+      this.data.client.subscribe('/light/deviceOut', function(err, granted) {
+        if (!err) {
+          wx.showToast({
+            title: '订阅主题成功'
+          })
+        } else {
+          wx.showToast({
+            title: '订阅主题失败',
+            icon: 'fail',
+            duration: 2000
           })
         }
-      }
-    })
+      })
+    } else {
+      wx.showToast({
+        title: '请先连接服务器',
+        icon: 'none',
+        duration: 2000
+      })
+    }
   },
+  onLoad: function() {
+    _this = this
+    this.mqttConnect()
+    colorPickerCtx = wx.createCanvasContext('colorPicker');
+    colorPickerCtx.fillStyle = 'rgb(255, 255, 255)';
+    sliderCtx = wx.createCanvasContext('colorPickerSlider');
 
-  onGetUserInfo: function(e) {
-    if (!this.data.logged && e.detail.userInfo) {
-      this.setData({
-        logged: true,
-        avatarUrl: e.detail.userInfo.avatarUrl,
-        userInfo: e.detail.userInfo
+    let isInit = true;
+    wx.createSelectorQuery().select('#colorPicker').boundingClientRect(function(rect) {
+      _this.setData({
+        valueWidthOrHerght: rect.width,
+      })
+      if(isInit){
+        colorPickerCtx.fillRect(0, 0, rect.width, rect.height);
+        util.drawRing(colorPickerCtx, rect.width, rect.height);
+        // 设置默认位置
+        util.drawSlider(sliderCtx, rect.width, rect.height, 1.0);
+        isInit = false;
+      }
+      
+      _this.setData({
+        pickColor: JSON.stringify({
+          red: 255,
+          green: 0,
+          blue: 0
+        })
+      })
+    }).exec();
+  },
+  mqttPubMsg: function(payload) {
+    if (this.data.client && this.data.client.connected) {
+      this.data.client.publish('/light/deviceIn', payload);
+    } else {
+      wx.showToast({
+        title: '请先连接服务器',
+        icon: 'none',
+        duration: 2000
       })
     }
   },
 
-  onGetOpenid: function() {
-    // 调用云函数
-    wx.cloud.callFunction({
-      name: 'login',
-      data: {},
-      success: res => {
-        console.log('[云函数] [login] user openid: ', res.result.openid)
-        app.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: '../userConsole/userConsole',
-        })
-      },
-      fail: err => {
-        console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
-      }
+  onClickRedColor: function() {
+
+    let obj = {
+      "change": "pwm",
+      "value": [255, 0, 0]
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
+    let h = util.rgb2hsl(255, 0, 0);
+    util.drawSlider(sliderCtx, _this.data.valueWidthOrHerght, _this.data.valueWidthOrHerght, h[0]);
+    this.setData({
+      pickColor: JSON.stringify({
+        red: 255,
+        green: 0,
+        blue: 0
+      })
     })
   },
 
-  // 上传图片
-  doUpload: function () {
-    // 选择图片
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
+  onClickGreenColor: function() {
+    let obj = {
+      "change": "pwm",
+      "value": [0, 255, 0]
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
 
-        wx.showLoading({
-          title: '上传中',
-        })
+    let h = util.rgb2hsl(0, 255, 0);
+    util.drawSlider(sliderCtx, _this.data.valueWidthOrHerght, _this.data.valueWidthOrHerght, h[0]);
+    this.setData({
+      pickColor: JSON.stringify({
+        red: 0,
+        green: 255,
+        blue: 0
+      })
+    })
+  },
 
-        const filePath = res.tempFilePaths[0]
-        
-        // 上传图片
-        const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
-        wx.cloud.uploadFile({
-          cloudPath,
-          filePath,
-          success: res => {
-            console.log('[上传文件] 成功：', res)
+  onClickBlueColor: function() {
+    let obj = {
+      "change": "pwm",
+      "value": [0, 0, 255]
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
 
-            app.globalData.fileID = res.fileID
-            app.globalData.cloudPath = cloudPath
-            app.globalData.imagePath = filePath
-            
-            wx.navigateTo({
-              url: '../storageConsole/storageConsole'
+    let h = util.rgb2hsl(0, 0, 255);
+    util.drawSlider(sliderCtx, _this.data.valueWidthOrHerght, _this.data.valueWidthOrHerght, h[0]);
+    this.setData({
+      pickColor: JSON.stringify({
+        red: 0,
+        green: 0,
+        blue: 255
+      })
+    })
+  },
+  onClickOpen:function(){
+    let obj  = {
+      "change": "power",
+      "value": "true"
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
+  },
+  onClickOff:function(){
+    let obj = {
+      "change": "power",
+      "value": "false"
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
+  },
+  onClickSync:function(){
+     let obj = {
+      "change": "query",
+      "value": "false"
+    }
+    this.mqttPubMsg(JSON.stringify(obj))
+  },
+  onSlide: function(e) {
+    let that = this;
+    if (e.touches && ( e.type === 'touchend')) {
+      console.log("ok");
+      let x = e.changedTouches[0].x;
+      let y = e.changedTouches[0].y;
+      if (e.type !== 'touchend') {
+        x = e.touches[0].x;
+        y = e.touches[0].y;
+      }
+      //复制画布上指定矩形的像素数据
+      wx.canvasGetImageData({
+        canvasId: "colorPicker",
+        x: x,
+        y: y,
+        width: 1,
+        height: 1,
+        success(res) {
+
+         
+          // 转换成hsl格式，获取旋转角度
+          let h = util.rgb2hsl(res.data[0], res.data[1], res.data[2]);
+          that.setData({
+            pickColor: JSON.stringify({
+              red: res.data[0],
+              green: res.data[1],
+              blue: res.data[2]
             })
-          },
-          fail: e => {
-            console.error('[上传文件] 失败：', e)
-            wx.showToast({
-              icon: 'none',
-              title: '上传失败',
-            })
-          },
-          complete: () => {
-            wx.hideLoading()
+          })
+          // 判断是否在圈内
+          if (h[1] !== 1.0) {
+            return;
           }
-        })
+          let obj = {
+            "change": "pwm",
+            "value": [res.data[0], res.data[1], res.data[2]]
+          }
+          that.mqttPubMsg(JSON.stringify(obj))
 
-      },
-      fail: e => {
-        console.error(e)
-      }
-    })
-  },
 
+          util.drawSlider(sliderCtx, _this.data.valueWidthOrHerght, _this.data.valueWidthOrHerght, h[0]);
+          // 设置设备
+          if (e.type !== 'touchEnd') {
+            // 触摸结束才设置设备属性
+            return;
+          }
+        }
+      });
+    }
+  }
 })
