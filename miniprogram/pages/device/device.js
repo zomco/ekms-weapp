8c335bb40240f85f21505d5157ade3830c5ac3cb
zomco <zomcoo@gmail.com>
2020-09-02 18:15:35 +0800

    修改设备页面

--- miniprogram/pages/device/device.js
+++ miniprogram/pages/device/device.js
@@ -4,28 +4,26 @@ const util = require('../../utils/util.js');
 Page({
   data: {
     name: '',
-    connectedDeviceId: '',
+    deviceId: '',
     serviceId: 0,
-    characteristics: {},
-    connected: true,
+    charactiseristics: {},
+    isConnected: true,
 
     pickColor: null,
-    raduis: 550, //这里最大为750rpx铺满屏幕
+    raduis: 550,        //这里最大为750rpx铺满屏幕
     valueWidthOrHerght: 0,
-    client: null,
-    reconnectCounts: 0, //记录重连的次数
   },
 
   SendTap: function() {
     var that = this
-    if (that.data.connected) {
+    if (that.data.isConnected) {
       var buffer = new ArrayBuffer(that.data.inputText.length)
       var dataView = new Uint8Array(buffer)
       for (var i = 0; i < that.data.inputText.length; i++) {
         dataView[i] = that.data.inputText.charCodeAt(i)
       }
       wx.writeBLECharacteristicValue({
-        deviceId: that.data.connectedDeviceId,
+        deviceId: that.data.deviceId,
         serviceId: that.data.serviceId,
         characteristicId: that.data.characteristics[0].uuid,
         value: buffer,
@@ -47,11 +45,53 @@ Page({
     }
   },
 
+  handleSlide: function(e) {
+    let that = this;
+    if (e.touches && ( e.type === 'touchend')) {
+      let x = e.changedTouches[0].x;
+      let y = e.changedTouches[0].y;
+      if (e.type !== 'touchend') {
+        x = e.touches[0].x;
+        y = e.touches[0].y;
+      }
+      //复制画布上指定矩形的像素数据
+      wx.canvasGetImageData({
+        canvasId: "colorPicker",
+        x: x,
+        y: y,
+        width: 1,
+        height: 1,
+        success(res) {
+          // 转换成hsl格式，获取旋转角度
+          let h = util.rgb2hsl(res.data[0], res.data[1], res.data[2]);
+          that.setData({
+            pickColor: JSON.stringify({
+              red: res.data[0],
+              green: res.data[1],
+              blue: res.data[2]
+            })
+          })
+          // 判断是否在圈内
+          if (h[1] !== 1.0) {
+            return;
+          }
+
+          util.drawSlider(sliderCtx, that.data.valueWidthOrHerght, that.data.valueWidthOrHerght, h[0]);
+          // 设置设备
+          if (e.type !== 'touchEnd') {
+            // 触摸结束才设置设备属性
+            return;
+          }
+        }
+      });
+    }
+  },
+
   onLoad: function(options) {
     var that = this
     that.setData({
       name: options.name,
-      connectedDeviceId: options.connectedDeviceId,
+      deviceId: options.deviceId,
     });
     colorPickerCtx = wx.createCanvasContext('colorPicker');
     colorPickerCtx.fillStyle = 'rgb(255, 255, 255)';
@@ -60,9 +100,7 @@ Page({
     // 绘制色环
     let isInit = true;
     wx.createSelectorQuery().select('#colorPicker').boundingClientRect(function(rect) {
-      that.setData({
-        valueWidthOrHerght: rect.width,
-      })
+      that.setData({ valueWidthOrHerght: rect.width });
       if(isInit){
         colorPickerCtx.fillRect(0, 0, rect.width, rect.height);
         util.drawRing(colorPickerCtx, rect.width, rect.height);
@@ -81,20 +119,10 @@ Page({
 
     // 获取外围设备服务列表
     wx.getBLEDeviceServices({
-      deviceId: that.data.connectedDeviceId,
+      deviceId: that.data.deviceId,
       success: function(res) {
-        var all_UUID = res.services;
-        var index_uuid = -1;
-        var UUID_lenght = all_UUID.length;
-        /* 遍历服务数组 */
-        for (var index = 0; index < UUID_lenght; index++) {
-          var ergodic_UUID = all_UUID[index].uuid; //取出服务里面的UUID
-          /* 判断是否是我们需要的00010203-0405-0607-0809-0A0B0C0D1910*/
-          if (ergodic_UUID == '00010203-0405-0607-0809-0A0B0C0D1910') {
-            index_uuid = index;
-          };
-        };
-        if (index_uuid == -1) {
+        const lightService = res.services.find(n => n.uuid === '00010203-0405-0607-0809-0A0B0C0D1910');
+        if (!lightService) {
           wx.showModal({
             title: '提示',
             content: '非我方出售的设备',
@@ -106,19 +134,16 @@ Page({
             }
           })
         }
-        that.setData({
-          serviceId: res.services[index_uuid].uuid
-        })
+        that.setData({ serviceId: lightService.uuid });
         wx.getBLEDeviceCharacteristics({
-          deviceId: options.connectedDeviceId,
-          serviceId: res.services[index_uuid].uuid,
+          deviceId: options.deviceId,
+          serviceId: lightService.uuid,
           success: function(res) {
-            that.setData({
-              characteristics: res.characteristics
-            })
+            that.setData({ characteristics: res.characteristics });
+            // 监听特点变化
             wx.notifyBLECharacteristicValueChange({
               state: true,
-              deviceId: options.connectedDeviceId,
+              deviceId: options.deviceId,
               serviceId: that.data.serviceId,
               characteristicId: that.data.characteristics[0].uuid,
               success: function(res) {
@@ -126,8 +151,8 @@ Page({
               },
               fail(res) {
                 console.log(res)
-              }
-            })
+              },
+            });
           }
         })
       },
@@ -138,9 +163,7 @@ Page({
     // 监听蓝牙事件
     wx.onBLEConnectionStateChange(function(res) {
       const { connected, deviceId } = res;
-      that.setData({
-        isConnected: connected,
-      });
+      that.setData({ isConnected: connected });
     });
     wx.onBLECharacteristicValueChange(function(res) {
       console.log('接收到数据：' + app.buf2string(res.value))
@@ -153,8 +176,13 @@ Page({
 
   onUnload: function() {
     wx.closeBLEConnection({
-      deviceId: this.data.connectedDeviceId,
-      success: function(res) {},
+      deviceId: this.data.deviceId,
+      success: function(res) {
+        console.log(res);
+      },
+      fail: function(err) {
+        console.log(err);
+      },
     })
   },
 
